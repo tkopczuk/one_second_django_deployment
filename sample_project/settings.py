fe6d592c47a790dc30f747f150f15ecf3b46cd34
Tomek Kopczuk <tkopczuk@gmail.com>
2011-07-23 23:07:15 +0200

    settings

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'askthepony',                      # Or path to database file if using sqlite3.
+        'USER': 'askthepony',                      # Not used with sqlite3.
+        'PASSWORD': 'askthepony',                  # Not used with sqlite3.
+        'HOST': '192.168.2.67',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
